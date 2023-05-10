0903a2030dc2d47cc70abeeacec9a67ea4204d0b
Sepitas <sebaaletkd@gmail.com>
2023-05-10 18:14:17 -0300

    Primer update

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    RedesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
