46067308b48fb23ac411bf0529ec1184bda99d78
Sepitas <129785223+Sepitas@users.noreply.github.com>
2023-05-13 23:00:16 -0300

    Casi terminado front

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { RedesComponent } from './components/redes/redes.component';
-import { LoginComponent } from './components/login/login.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaComponent } from './components/acerca/acerca.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,8 +18,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoComponent,
-    RedesComponent,
-    LoginComponent,
     BannerComponent,
     AcercaComponent,
     ExperienciaComponent,
