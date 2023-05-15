2829a63a8be1e2cf44b8cf314fa917c3bf3bbade
Sepitas <129785223+Sepitas@users.noreply.github.com>
2023-05-15 02:57:40 -0300

    Se agrego bas de datos

--- ProyectoFinal/Back end/src/main/java/com/portafolio/seba/Controlador/PersonaControl.java
+++ ProyectoFinal/Back end/src/main/java/com/portafolio/seba/Controlador/PersonaControl.java
@@ -5,6 +5,7 @@ import com.portafolio.seba.Entidad.Persona;
 import com.portafolio.seba.Interfaz.IPersonaServ;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/" )
 public class PersonaControl {
     @Autowired IPersonaServ ipersonaService;
     
@@ -55,5 +57,8 @@ public class PersonaControl {
     }
     
     
-  ;  
+    @GetMapping("/personas/traer/perfil") 
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
+    }
 }
